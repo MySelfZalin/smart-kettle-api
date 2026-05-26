09397715b7f66bbf6e442b28617195c1c84e6ba5
MySelfZalin <zalinn122@gmail.com>
2026-05-26 04:51:07 +0300

    feat:(secure) protect swagger docs and openapi with auth

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,11 +1,18 @@
+import secrets
 import uvicorn
 import logging
-from fastapi import FastAPI
 import sys
 from loguru import logger
+from fastapi import FastAPI, Depends, status, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 from fast_api.api.smart_kettle import kettle_router
 from fast_api.api.yandex_auth import yandex_auth_router
 from fast_api.api.yandex_smarthome import yandex_smarthome_router
+from config import settings
+
+
 
 
 class InterceptHandler(logging.Handler):
@@ -28,8 +35,14 @@ logger.add(sys.stderr, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <leve
 logger.add("api.log", rotation="5 MB", retention="10 days", encoding="utf-8", level="INFO")
 #================================================
 
+security = HTTPBasic()
 
-app = FastAPI(title="Smart-Kettle API")
+app = FastAPI(
+    title="Smart-Kettle API",
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None
+    )
 
 app.include_router(kettle_router)
 app.include_router(yandex_auth_router)
@@ -39,6 +52,30 @@ app.include_router(yandex_smarthome_router)
 @app.get("/")
 def read_root():
     return {"status": "API is working", "project": "smart-kettle-api"}
+
+
+
+def verify_credentials(credentials: HTTPBasicCredentials = Depends(security)):
+    is_username_correct = secrets.compare_digest(credentials.username, settings.KETTLE_USERNAME)
+    is_password_correct = secrets.compare_digest(credentials.password, settings.KETTLE_PASSWORD)
+    
+    if not (is_username_correct and is_password_correct):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.get("/docs", include_in_schema=False)
+async def get_swagger_documentation(username: str = Depends(verify_credentials)):
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
+
+
+@app.get("/openapi.json", include_in_schema=False)
+async def openapi(username: str = Depends(verify_credentials)):
+    return get_openapi(title=app.title, version=app.version, routes=app.routes)
 
   
     
